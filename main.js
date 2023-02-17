7636b5106ec746142351ab2ae0ee4f491258bf9e
ishii-cgp <89733348+ishii-cgp@users.noreply.github.com>
2023-02-18 03:10:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function makeresult(vcs,rgy){
 function selectshop(ddmenu){
   switch(ddmenu){
     case '0':
-    var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vRYdUc-mpd3Lvy2ZSUd3xyMfB3avzXDv5kbzJU5SwPbhAv0yCqMGHYR4yhJfSFT96Vvc0GgpGcXw3JV/pub?gid=0&single=true&output=csv');
+    var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vT4Ts7CzrlubGBcNx8t3GSr71n2KttzWHYo6ngXv9Ij5_mgSWJR7pIlQG0IlP8AgQMzUZ3iXbYSgiD5/pub?output=csv');
     break;
     case '1':
     var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vQYpyRZCLhMy_9GpTtDY_EGeyusR18Td38iunmnykoRMgCFLnzLU16cH6VnwUbPUplTzZZZ93GNJRa_/pub?gid=0&single=true&output=csv');
