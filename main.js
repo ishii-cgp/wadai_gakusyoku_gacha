41f4113eb5c964ee4c2075f23d05038a224e9d33
ishii-cgp <89733348+ishii-cgp@users.noreply.github.com>
2023-08-15 15:30:28 +0900

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ function selectshop(ddmenu){
     var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vT5IQKs6FhO896C7H8xXFk6oGiN9xToQFTc2Ix5HTKSxKId2FBn_oeIxAzysd7bNx-7wZ5SHpMffKzM/pub?output=csv');
     break;
     case '1':
-    var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vQ8lcvLlu9vC2SVG2DaGEQULaMXlj-rlzdp15tJilWH9h01qU35DQblaoooc9OChkrwqUjcVjtK6JW-/pub?output=csv');
+    var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vRYdUc-mpd3Lvy2ZSUd3xyMfB3avzXDv5kbzJU5SwPbhAv0yCqMGHYR4yhJfSFT96Vvc0GgpGcXw3JV/pub?gid=0&single=true&output=csv');
     break;
     default:
     var data = csvToArray('https://docs.google.com/spreadsheets/d/e/2PACX-1vRYdUc-mpd3Lvy2ZSUd3xyMfB3avzXDv5kbzJU5SwPbhAv0yCqMGHYR4yhJfSFT96Vvc0GgpGcXw3JV/pub?gid=0&single=true&output=csv');
